4014509ceb71ec9f6f150d84b9f466af841856e0
Josh Francis <joshua.francis@live.com.au>
2019-06-17 10:02:10 +1000

    Project upload.
    Created the project with the bootstrap and uploaded.

---
+++ project2D/Application2D.cpp
@@ -0,0 +1,57 @@
+#include "Application2D.h"
+#include "Texture.h"
+#include "Font.h"
+#include "Input.h"
+
+Application2D::Application2D() {
+
+}
+
+Application2D::~Application2D() {
+
+}
+
+bool Application2D::startup() {
+	
+	m_2dRenderer = new aie::Renderer2D();
+
+
+	m_font = new aie::Font("./font/consolas.ttf", 32);
+	
+	m_timer = 0;
+
+	return true;
+}
+
+void Application2D::shutdown() {
+	
+	delete m_font;
+	delete m_2dRenderer;
+}
+
+void Application2D::update(float deltaTime) {
+
+	m_timer += deltaTime;
+
+	// input example
+	aie::Input* input = aie::Input::getInstance();
+
+
+	// exit the application
+	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
+		quit();
+}
+
+void Application2D::draw() {
+
+	// wipe the screen to the background colour
+	clearScreen();
+
+	// begin drawing sprites
+	m_2dRenderer->begin();
+
+	
+
+	// done drawing sprites
+	m_2dRenderer->end();
+}
